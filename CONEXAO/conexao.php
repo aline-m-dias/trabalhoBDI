f79cd8b1422e7366ec740f9a2a345b2e665036bc
naalmeida98 <natii_98_@hotmail.com>
2021-07-31 18:43:23 -0300

    Conexão com BD e pequenas atualizações no front-end

---
+++ CONEXAO/conexao.php
@@ -0,0 +1,26 @@
+<?php
+
+class Conexao {
+
+	private $host = 'localhost';
+	private $dbname = 'poupeMais';
+	private $user = 'postgres';
+	private $pass = '1234';
+
+	public function conectar() {
+		try {
+
+			$conexao = new PDO(
+				"pgsql:host=$this->host;dbname=$this->dbname",
+				"$this->user",
+				"$this->pass"				
+			);
+
+			return $conexao;
+		} catch (PDOException $e) {
+			echo '<p>'.$e->getMessage().'</p>';
+		}
+	}
+}
+
+?>
---
+++ CONEXAO/servicos.bd.php
@@ -0,0 +1,40 @@
+<?php
+
+
+
+class TarefaService {
+
+	private $conexao;
+	private $tarefa;
+
+	public function __construct(Conexao $conexao, Tarefa $tarefa) {
+		$this->conexao = $conexao->conectar();
+		$this->tarefa = $tarefa;
+	}
+
+	public function inserir() { //create
+		$query = 'insert into tb_tarefas(tarefa)values(:tarefa)';
+		$stmt = $this->conexao->prepare($query); 
+		$stmt->bindValue(':tarefa', $this->tarefa->__get('tarefa'));
+		$stmt->execute();
+	}
+
+	public function recuperar() { //read
+		$query = '
+			select 
+				t.id, s.status, t.tarefa 
+			from 
+				tb_tarefas as t
+				left join tb_status as s on (t.id_status = s.id)
+		';
+		$stmt = $this->conexao->prepare($query);
+		$stmt->execute();
+		return $stmt->fetchAll(PDO::FETCH_OBJ); // fetchall retorna todos os registros da consulta em forrma
+		// em forma de um array, para acessar indice especifico variavel[]->local 
+		//TECH_OBJ retorna a consulta em classes de objetos
+	}
+
+	
+}
+
+?>
