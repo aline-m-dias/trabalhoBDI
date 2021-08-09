816b3ffee24a6c3b85bba0eb922d8e0f87b2c5ba
naalmeida98 <natii_98_@hotmail.com>
2021-08-09 13:43:49 -0300

    Atualizações

--- despesa.php
+++ despesa.php
@@ -13,11 +13,6 @@
             $nome_despesa[$i]['valor'] = isset( $listaDespesas[$i]['valor']) ?  $listaDespesas[$i]['valor'] : $listaDespesas[$i]['valor'];
             $nome_despesa[$i]['data_desp'] = isset( $listaDespesas[$i]['data_desp']) ?  $listaDespesas[$i]['data_desp'] : $listaDespesas[$i]['data_desp'];
         }
-        $tam = count($listaDespesas);
-        echo $tam;
-        echo '<prep>';
-        print_r($listaDespesas);
-        echo '</prep>';
     }
     ?>
 <!DOCTYPE html>
@@ -47,42 +42,40 @@
         <p class="logar">Despesa</p>
 
         <form id="register-form-despesa" action="controle_servico_despesa.php?acao=inserirDespesa" method="post" name="logar">
+            <div class="full-box">
+                <label for="name">Nome</label>
+                <input type="text" name="nome" id="nome" placeholder="Digite o nome">
+            </div>
+            <div class="full-box">
+                <label for="name">Valor</label>
+                <input type="number" step="0.01" name="valor" id="valor" placeholder="Digite o valor">
+            </div>
+            <div class="full-box">
+                <label for="name">Categorias</label>
+                <select name='tipo'>
+                    <option value="--">--</option>
+                    <option value="Alimentação">Alimentação</option>
+                    <option value="Saúde">Saúde</option>
+                    <option value="Educação">Educação</option>
+                    <option value="Moradia">Moradia</option>
+                    <option value="Transporte">Transporte</option>
+                    <option value="Diversos">Diversos</option>
+                </select>
+            </div>
+            <div class="full-box">
+                <label for="name">Data</label>
+                <input type="date" name="data_desp" id="data_desp">
+            </div>
 
-            <form id="register-form-despesa" action="controle_servico_despesa.php?acao=inserirDespesa" method="post" name="logar">
-                <div class="full-box">
-                    <label for="name">Nome</label>
-                    <input type="text" name="nome" id="nome" placeholder="Digite o nome">
-                </div>
-                <div class="full-box">
-                    <label for="name">Valor</label>
-                    <input type="number" step="0.01" name="valor" id="valor" placeholder="Digite o valor">
-                </div>
-                <div class="full-box">
-                    <label for="name">Categorias</label>
-                    <select name='tipo'>
-                        <option value="--">--</option>
-                        <option value="Alimentação">Alimentação</option>
-                        <option value="Saúde">Saúde</option>
-                        <option value="Educação">Educação</option>
-                        <option value="Moradia">Moradia</option>
-                        <option value="Transporte">Transporte</option>
-                        <option value="Diversos">Diversos</option>
-                    </select>
-                </div>
-                <div class="full-box">
-                    <label for="name">Data</label>
-                    <input type="date" name="data_desp" id="data_desp">
-                </div>
-
-                <div class="full-box">
-                    <input id="btn-submit" type="submit" value="Enviar dados">
-                </div>
-            </form>
-            <?php if (isset($_GET['despesacadastrada']) && $_GET['despesacadastrada'] == 1) { ?>
-                <div class="full-box">
-                    <h5>Despesa cadastrada com sucesso!</h5>
-                </div>
-            <?php } ?>
+            <div class="full-box">
+                <input id="btn-submit" type="submit" value="Enviar dados">
+            </div>
+        </form>
+        <?php if (isset($_GET['despesacadastrada']) && $_GET['despesacadastrada'] == 1) { ?>
+            <div class="full-box">
+                <h5>Despesa cadastrada com sucesso!</h5>
+            </div>
+        <?php } ?>
     </div>
     <div class="clear"></div>
     <div class="cadrastrarDespesa">
@@ -102,15 +95,17 @@
                 </select>
             </div>
             <div class="full-box">
-                    <input id="btn-submit" type="submit" value="Pesquisar">
+                <input id="btn-submit" type="submit" value="Pesquisar">
             </div>
         </form>
     </div>
     <div class="clear"></div>
+
     <div class="fundoPretoCadastro">
-    <div><h2>Despesas cadastradas<h2><div>
     <?php
-    if (isset($_GET['pesquisaDespesa']) && $_GET['pesquisaDespesa'] == 1) { 
+    if (isset($_GET['pesquisaDespesa']) && $_GET['pesquisaDespesa'] == 1) { ?>
+    <div><h2>Despesas cadastradas<h2><div>
+        <?php
         $i = isset($i) ? 0 : 0;
         for($i=0; $i<count($nome_despesa); $i++){ ?>
         <div>
