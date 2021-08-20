fe84a1fbfde6587d33591cc51591891b2aaf8003
Aline Dias <martinsaline558@gmail.com>
2021-08-19 23:22:54 -0300

    teste hospedagem

--- CONEXAO/conexao.php
+++ CONEXAO/conexao.php
@@ -1,25 +1,25 @@
 <?php
 
-class Conexao{
+class Conexao
+{
 
-	private $host = 'localhost';
-	private $dbname = 'poupemais';
-	private $user = 'postgres';
-	private $pass = '1234';
+	private $host = 'ec2-35-153-114-74.compute-1.amazonaws.com';
+	private $dbname = 'd46f2u52mcqvnk';
+	private $user = 'cfpffxorgxfkzk';
+	private $pass = '1c9ce944b50a8fa0afed83ddc6cf8229289db0f3fe6d911bd3dd0de47c3ead2f';
 
-	public function conectar() {
+	public function conectar()
+	{
 		try {
 			$conexao = new PDO(
 				"pgsql:host=$this->host;dbname=$this->dbname",
 				"$this->user",
-				"$this->pass"				
+				"$this->pass"
 			);
 			return $conexao;
 		} catch (PDOException $e) {
-			echo '<p>'.$e->getMessage().'</p>';
+			echo '<p>' . $e->getMessage() . '</p>';
 			print("ERRO DE CONEXÃO");
 		}
 	}
 }
-
-?>
