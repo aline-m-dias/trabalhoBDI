89d1114796cb69606044aca65862b4d5591c82d6
Aline Dias <martinsaline558@gmail.com>
2021-08-20 15:58:47 -0300

    teste banco de dados

--- CONEXAO/conexao.php
+++ CONEXAO/conexao.php
@@ -1,25 +1,26 @@
 <?php
 
-class Conexao{
+class Conexao
+{
+	private $host = 'ec2-44-195-247-84.compute-1.amazonaws.com';
+	private $dbname = 'dcdqohb6s9ia6e';
+	private $user = 'hfmgsqeodenguh';
+	private $pass = 'f33fedc0e4e7ec79ad2304303b21b31df0b336eeb81237861f44f55e64645593';
+	//private $heroku pg:psql;
 
-	private $host = 'localhost';
-	private $dbname = 'pmais';
-	private $user = 'postgres';
-	private $pass = '1234';
-
-	public function conectar() {
+	public function conectar()
+	{
 		try {
 			$conexao = new PDO(
+
 				"pgsql:host=$this->host;dbname=$this->dbname",
 				"$this->user",
-				"$this->pass"				
+				"$this->pass"
 			);
 			return $conexao;
 		} catch (PDOException $e) {
-			echo '<p>'.$e->getMessage().'</p>';
+			echo '<p>' . $e->getMessage() . '</p>';
 			print("ERRO DE CONEXÃO");
 		}
 	}
 }
-
-?>
